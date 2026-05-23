cf5776c54f2b6fe2debe8e0abd1a37ec4b16e8ee
Ethan Vukhoi Nguyen <ethanswer@gmail.com>
2026-05-23 15:18:38 -0700

    Added camera controller and affixed it to player

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+namespace Camera
+{
+    public class CameraController : MonoBehaviour
+    {
+        public GameObject player;
+        private Vector3 offset;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            offset = transform.position - player.transform.position;   
+        }
+
+        private void LateUpdate()
+        {
+            transform.position = player.transform.position + offset;
+        }
+    }
+}
