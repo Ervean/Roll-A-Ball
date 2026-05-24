3b88818e9b37592d545b2991a1200c9e4cc6bdee
Ethan Vukhoi Nguyen <ethanswer@gmail.com>
2026-05-24 01:55:41 -0700

    Added gameplay, can now win after picking up all items on field.

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -1,20 +1,28 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 namespace Player
 {
     public class PlayerController : MonoBehaviour
     {
+        public TextMeshProUGUI countText;
+        public TextMeshProUGUI winText;
         public float Speed = 10f;
         private Rigidbody rb;
         private float movementX;
         private float movementY;
+        private int count;
 
+        private const int MaxPickUp = 7;
 
         // Start is called once before the first execution of Update after the MonoBehaviour is created
         private void Start()
         {
+            count = 0;
             rb = GetComponent<Rigidbody>();    
+            SetCountText();
+            winText.gameObject.SetActive(false);
         }
 
         private void FixedUpdate()
@@ -26,6 +34,15 @@ namespace Player
 
         }
 
+        private void OnTriggerEnter(Collider other)
+        {
+            if(other.gameObject.CompareTag("PickUp"))
+            {
+                other.gameObject.SetActive(false);        
+                count++;  
+                SetCountText();
+            }
+        }
 
         private void OnMove (InputValue movementValue)
         {
@@ -34,6 +51,15 @@ namespace Player
             movementY = movementVector.y;
         }
         
+        private void SetCountText()
+        {
+            countText.text = "Count: " + count.ToString();
+
+            if(count >= MaxPickUp)
+            {
+                winText.gameObject.SetActive(true);
+            }
+        }
     
     }
 }
